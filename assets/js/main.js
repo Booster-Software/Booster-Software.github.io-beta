b901b6121d573e5755642d8cbb35bd8f99ec3c86
JuanBooster <150477907+JuanBooster@users.noreply.github.com>
2023-11-30 17:17:59 -0300

    Feature/booster factory (#5)

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,47 +26,57 @@
         scrollTop: 0,
       },
       1500,
-      "easeInOutExpo",
+      "easeInOutExpo"
     );
     return false;
   });
 
   // Smooth scroll for the navigation menu and links with .scrollto classes
-  var scrolltoOffset = $("#header").outerHeight() - 17;
-  $(document).on("click", ".nav-menu a, .mobile-nav a, .scrollto", function (e) {
-    if (location.pathname.replace(/^\//, "") == this.pathname.replace(/^\//, "") && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      if (target.length) {
-        e.preventDefault();
+  var scrolltoOffset = $(".header").outerHeight() - 17;
+  $(document).on(
+    "click",
+    ".nav-menu a, .mobile-nav a, .scrollto",
+    function (e) {
+      if (
+        location.pathname.replace(/^\//, "") ==
+          this.pathname.replace(/^\//, "") &&
+        location.hostname == this.hostname
+      ) {
+        var target = $(this.hash);
+        if (target.length) {
+          e.preventDefault();
 
-        var scrollto = target.offset().top - scrolltoOffset;
+          var scrollto = target.offset().top - scrolltoOffset;
 
-        if ($(this).attr("href") == "#header") {
-          scrollto = 0;
+          if ($(this).attr("href") == ".header") {
+            scrollto = 0;
+          }
+
+          $("html, body").animate(
+            {
+              scrollTop: scrollto,
+            },
+            1500,
+            "easeInOutExpo"
+          );
+
+          if ($(this).parents(".nav-menu, .mobile-nav").length) {
+            $(".nav-menu .active, .mobile-nav .active").removeClass("active");
+            $(this).closest("li").addClass("active");
+          }
+
+          if ($("body").hasClass("mobile-nav-active")) {
+            $("body").removeClass("mobile-nav-active");
+            $(".mobile-nav-toggle i").toggleClass(
+              "icofont-navigation-menu icofont-close"
+            );
+            $(".mobile-nav-overly").fadeOut();
+          }
+          return false;
         }
-
-        $("html, body").animate(
-          {
-            scrollTop: scrollto,
-          },
-          1500,
-          "easeInOutExpo",
-        );
-
-        if ($(this).parents(".nav-menu, .mobile-nav").length) {
-          $(".nav-menu .active, .mobile-nav .active").removeClass("active");
-          $(this).closest("li").addClass("active");
-        }
-
-        if ($("body").hasClass("mobile-nav-active")) {
-          $("body").removeClass("mobile-nav-active");
-          $(".mobile-nav-toggle i").toggleClass("icofont-navigation-menu icofont-close");
-          $(".mobile-nav-overly").fadeOut();
-        }
-        return false;
       }
     }
-  });
+  );
 
   // Activate smooth scroll on page load with hash links in the url
   $(document).ready(function () {
@@ -79,7 +89,7 @@
             scrollTop: scrollto,
           },
           1500,
-          "easeInOutExpo",
+          "easeInOutExpo"
         );
       }
     }
@@ -91,12 +101,16 @@
       class: "mobile-nav d-lg-none",
     });
     $("body").append($mobile_nav);
-    $("body").prepend('<button type="button" class="mobile-nav-toggle d-lg-none"><i class="icofont-navigation-menu"></i></button>');
+    $("body").prepend(
+      '<button type="button" class="mobile-nav-toggle d-lg-none"><i class="icofont-navigation-menu"></i></button>'
+    );
     $("body").append('<div class="mobile-nav-overly"></div>');
 
     $(document).on("click", ".mobile-nav-toggle", function (e) {
       $("body").toggleClass("mobile-nav-active");
-      $(".mobile-nav-toggle i").toggleClass("icofont-navigation-menu icofont-close");
+      $(".mobile-nav-toggle i").toggleClass(
+        "icofont-navigation-menu icofont-close"
+      );
       $(".mobile-nav-overly").toggle();
     });
 
@@ -111,7 +125,9 @@
       if (!container.is(e.target) && container.has(e.target).length === 0) {
         if ($("body").hasClass("mobile-nav-active")) {
           $("body").removeClass("mobile-nav-active");
-          $(".mobile-nav-toggle i").toggleClass("icofont-navigation-menu icofont-close");
+          $(".mobile-nav-toggle i").toggleClass(
+            "icofont-navigation-menu icofont-close"
+          );
           $(".mobile-nav-overly").fadeOut();
         }
       }
@@ -123,14 +139,14 @@
   // Header scroll class
   $(window).scroll(function () {
     if ($(this).scrollTop() > 100) {
-      $("#header").addClass("header-scrolled");
+      $(".header").addClass("header-scrolled");
     } else {
-      $("#header").removeClass("header-scrolled");
+      $(".header").removeClass("header-scrolled");
     }
   });
 
   if ($(window).scrollTop() > 100) {
-    $("#header").addClass("header-scrolled");
+    $(".header").addClass("header-scrolled");
   }
 
   // Navigation active state on scroll
@@ -167,13 +183,23 @@
     .children(".carousel-item")
     .each(function (index) {
       index === 0
-        ? introCarouselIndicators.append("<li data-target='#introCarousel' data-slide-to='" + index + "' class='active'></li>")
-        : introCarouselIndicators.append("<li data-target='#introCarousel' data-slide-to='" + index + "'></li>");
+        ? introCarouselIndicators.append(
+            "<li data-target='#introCarousel' data-slide-to='" +
+              index +
+              "' class='active'></li>"
+          )
+        : introCarouselIndicators.append(
+            "<li data-target='#introCarousel' data-slide-to='" +
+              index +
+              "'></li>"
+          );
     });
 
   introCarousel.on("slid.bs.carousel", function (e) {
     $(this).find("h2").addClass("animate__animated animate__fadeInDown");
-    $(this).find("p, .btn-get-started").addClass("animate__animated animate__fadeInUp");
+    $(this)
+      .find("p, .btn-get-started")
+      .addClass("animate__animated animate__fadeInUp");
   });
 
   // Skills section
@@ -185,7 +211,7 @@
     },
     {
       offset: "80%",
-    },
+    }
   );
 
   // jQuery counterUp (used in Facts section)
@@ -317,14 +343,26 @@ function showSubmitResult(result) {
   getTranslation(function (language) {
     if (result == 200) {
       $("#careersSubmitCollapseAlert").addClass("alert-success");
-      $("#careersSubmitCollapseAlertHeader").html(language.careersSubmitCollapseAlertHeaderOk);
-      $("#careersSubmitCollapseAlertMainMessage").html(language.careersSubmitCollapseAlertMainMessageOk);
-      $("#careersSubmitCollapseAlertFooterMessage").html(language.careersSubmitCollapseAlertFooterMessageOk);
+      $("#careersSubmitCollapseAlertHeader").html(
+        language.careersSubmitCollapseAlertHeaderOk
+      );
+      $("#careersSubmitCollapseAlertMainMessage").html(
+        language.careersSubmitCollapseAlertMainMessageOk
+      );
+      $("#careersSubmitCollapseAlertFooterMessage").html(
+        language.careersSubmitCollapseAlertFooterMessageOk
+      );
     } else {
       $("#careersSubmitCollapseAlert").addClass("alert-danger");
-      $("#careersSubmitCollapseAlertHeader").html(language.careersSubmitCollapseAlertHeaderError);
-      $("#careersSubmitCollapseAlertMainMessage").html(language.careersSubmitCollapseAlertMainMessageError);
-      $("#careersSubmitCollapseAlertFooterMessage").html(language.careersSubmitCollapseAlertFooterMessageError);
+      $("#careersSubmitCollapseAlertHeader").html(
+        language.careersSubmitCollapseAlertHeaderError
+      );
+      $("#careersSubmitCollapseAlertMainMessage").html(
+        language.careersSubmitCollapseAlertMainMessageError
+      );
+      $("#careersSubmitCollapseAlertFooterMessage").html(
+        language.careersSubmitCollapseAlertFooterMessageError
+      );
     }
     $("#careersSubmitCollapse").collapse("show");
   });
@@ -367,11 +405,15 @@ function setLanguage(newLang) {
 }
 
 function setActiveFlag(lang) {
-  document.getElementById("languageFlag" + (lang || "en").toUpperCase()).classList.add("active-language-flag");
+  document
+    .getElementById("languageFlag" + (lang || "en").toUpperCase())
+    .classList.add("active-language-flag");
 }
 
 function setInactiveFlag(lang) {
-  document.getElementById("languageFlag" + (lang || "en").toUpperCase()).classList.remove("active-language-flag");
+  document
+    .getElementById("languageFlag" + (lang || "en").toUpperCase())
+    .classList.remove("active-language-flag");
 }
 
 function translate() {
@@ -380,6 +422,13 @@ function translate() {
     $("#navMenuHome").html(language.home);
     $("#navMenuAboutUs").html(language.aboutUs);
     $("#navMenuServices").html(language.services);
+    $("#navMenuServices1").html(language.servicesTitle1);
+    $("#navMenuServices2").html(language.servicesTitle2);
+    $("#navMenuServices3").html(language.servicesTitle3);
+    $("#navMenuServices4").html(language.servicesTitle4);
+    $("#navMenuServices5").html(language.servicesTitle5);
+    $("#navMenuServices6").html(language.navMenuFactoryCustomSoftware);
+    $("#navMenuServices7").html(language.navMenuFactoryEcommerce);
     $("#navMenuCareers").html(language.careers);
     $("#navMenuContactUs").html(language.contactUs);
     $("#homeHeader1Text1").html(language.homeHeader1Text1);
@@ -388,14 +437,23 @@ function translate() {
     $("#homeHeader2Text1").html(language.homeHeader2Text1);
     $("#homeHeader2Text2").html(language.homeHeader2Text2);
     $("#homeButton2").html(language.homeButton2);
+    $("#homeHeader3Text1").html(language.homeHeader3Text1);
+    $("#homeHeader3Text2").html(language.homeHeader3Text2);
+    $("#homeButton3").html(language.homeButton3);
     $("#homeCarouselPrevious").html(language.homeCarouselPrevious);
     $("#homeCarouselNext").html(language.homeCarouselNext);
     $("#featuredServicesTitle1").html(language.featuredServicesTitle1);
-    $("#featuredServicesDescription1").html(language.featuredServicesDescription1);
+    $("#featuredServicesDescription1").html(
+      language.featuredServicesDescription1
+    );
     $("#featuredServicesTitle2").html(language.featuredServicesTitle2);
-    $("#featuredServicesDescription2").html(language.featuredServicesDescription2);
+    $("#featuredServicesDescription2").html(
+      language.featuredServicesDescription2
+    );
     $("#featuredServicesTitle3").html(language.featuredServicesTitle3);
-    $("#featuredServicesDescription3").html(language.featuredServicesDescription3);
+    $("#featuredServicesDescription3").html(
+      language.featuredServicesDescription3
+    );
     $("#aboutUsDescription").html(language.aboutUsDescription);
     $("#aboutUsTitle").html(language.aboutUs);
     $("#aboutUsTitle1").html(language.aboutUsTitle1);
@@ -417,7 +475,9 @@ function translate() {
     $("#servicesTitle5").html(language.servicesTitle5);
     $("#servicesDescription5").html(language.servicesDescription5);
     $("#letsWorkTogetherTitle").html(language.letsWorkTogetherTitle);
-    $("#letsWorkTogetherDescription").html(language.letsWorkTogetherDescription);
+    $("#letsWorkTogetherDescription").html(
+      language.letsWorkTogetherDescription
+    );
     $("#letsWorkTogetherCTA").html(language.contactUs);
     $("#ourClients").html(language.ourClients);
     $("#teamTitle").html(language.teamTitle);
@@ -443,6 +503,53 @@ function translate() {
     $("#careersCVemailLabel").html(language.careersCVemailLabel);
     $("#careersCVfileLabel").html(language.careersCVfileLabel);
     $("#careersSubmitLabel").html(language.careersSubmitLabel);
+    // Booster Factory
+    $("#navMenuFactory").html(language.navMenuFactory);
+    $("#navMenuFactoryCustomSoftware").html(language.navMenuFactoryCustomSoftware);
+    $("#navMenuFactoryEcommerce").html(language.navMenuFactoryEcommerce);
+    $("#factoryMainTitleText1").html(language.factoryMainTitleText1);
+    $("#factoryMainTitleText2").html(language.factoryMainTitleText2);
+    $("#factoryFeatures1Feature1").html(language.factoryFeatures1Feature1);
+    $("#factoryFeatures1Feature2").html(language.factoryFeatures1Feature2);
+    $("#factoryFeatures1Feature3").html(language.factoryFeatures1Feature3);
+    $("#factoryFeatures2Feature1").html(language.factoryFeatures2Feature1);
+    $("#factoryFeatures2Feature2").html(language.factoryFeatures2Feature2);
+    $("#factoryFeatures2Feature3").html(language.factoryFeatures2Feature3);
+    $("#factoryFeatures3Feature1").html(language.factoryFeatures3Feature1);
+    $("#factoryFeatures3Feature2").html(language.factoryFeatures3Feature2);
+    $("#factoryFeatures3Feature3").html(language.factoryFeatures3Feature3);
+    $("#factoryCustomSoftwareTitle").html(language.factoryCustomSoftwareTitle);
+    $("#factoryCustomSoftwareDescription").html(language.factoryCustomSoftwareDescription);
+    $("#factoryCustomSoftwareTitle1").html(language.factoryCustomSoftwareTitle1);
+    $("#factoryCustomSoftwareDescription1").html(language.factoryCustomSoftwareDescription1);
+    $("#factoryCustomSoftwareTitle2").html(language.factoryCustomSoftwareTitle2);
+    $("#factoryCustomSoftwareDescription2").html(language.factoryCustomSoftwareDescription2);
+    $("#factoryCustomSoftwareTitle3").html(language.factoryCustomSoftwareTitle3);
+    $("#factoryCustomSoftwareDescription3").html(language.factoryCustomSoftwareDescription3);
+    $("#factoryCustomSoftwareTitle4").html(language.factoryCustomSoftwareTitle4);
+    $("#factoryCustomSoftwareDescription4").html(language.factoryCustomSoftwareDescription4);
+    $("#factoryCustomSoftwareTitle5").html(language.factoryCustomSoftwareTitle5);
+    $("#factoryCustomSoftwareDescription5").html(language.factoryCustomSoftwareDescription5);
+    $("#factoryCustomSoftwareTitle6").html(language.factoryCustomSoftwareTitle6);
+    $("#factoryCustomSoftwareDescription6").html(language.factoryCustomSoftwareDescription6);
+    $("#eCommerceDescription").html(language.eCommerceDescription);
+    $("#ecommerceMediaTile1").html(language.ecommerceMediaTile1);
+    $("#ecommerceMediaDescription1").html(language.ecommerceMediaDescription1);
+    $("#ecommerceMediaTile2").html(language.ecommerceMediaTile2);
+    $("#ecommerceMediaDescription2").html(language.ecommerceMediaDescription2);
+    $("#ecommerceMediaTile3").html(language.ecommerceMediaTile3);
+    $("#ecommerceMediaDescription3").html(language.ecommerceMediaDescription3);
+    $("#ecommerceMediaTile4").html(language.ecommerceMediaTile4);
+    $("#ecommerceMediaDescription4").html(language.ecommerceMediaDescription4);
+    $("#ecommerceMediaTile5").html(language.ecommerceMediaTile5);
+    $("#ecommerceMediaDescription5").html(language.ecommerceMediaDescription5);
+    $("#ecommerceMediaTile6").html(language.ecommerceMediaTile6);
+    $("#ecommerceMediaDescription6").html(language.ecommerceMediaDescription6);
+    $("#ecommerceMediaTile7").html(language.ecommerceMediaTile7);
+    $("#ecommerceMediaDescription7").html(language.ecommerceMediaDescription7);
+    $("#digitalPartnerTitle").html(language.digitalPartnerTitle);
+    $("#digitalPartnerDescription").html(language.digitalPartnerDescription);
+    $("#digitalPartnerCTA").html(language.digitalPartnerCTA);
   });
 }
 
@@ -450,3 +557,8 @@ $(document).ready(function () {
   translate();
   setActiveFlag(localStorage.getItem("language"));
 });
+
+// Booster Factory fancy background
+$(".fancyCubesBackground").append(
+  '<div><ul class="cubes">' + "<li></li>".repeat(10) + "</ul></div>"
+);
