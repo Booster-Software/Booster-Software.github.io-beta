838ccac003870ff9c8173b8383836df565bf2ba7
Gabriel Monteagudo <gabrielmaldi@hotmail.com>
2023-12-08 16:48:21 -0300

    Cache buster

--- assets/js/main.js
+++ assets/js/main.js
@@ -410,7 +410,7 @@ function getTranslation(cb) {
   var lang = localStorage.getItem("language") || "en";
 
   $.ajax({
-    url: "/assets/languages/" + lang + ".json?v=10",
+    url: "/assets/languages/" + lang + ".json?v=11",
     dataType: "json",
     success: function (lang) {
       cb(lang);
